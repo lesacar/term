61a24bcfafa3e97748edcb40f3417d5b7b0c6244
lesacar <69954403+lesacar@users.noreply.github.com>
2025-01-12 21:22:39 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <cstdio>
 #include <raylib.h>
 
+void draw_frame() {
+    DrawText(TextFormat("%d", GetFPS()), 20, 20, 40, RED);
+    DrawText(TextFormat("%d", 123456789), 20, 70, 40, RED);
+    // DrawTriangle(Vector2{1600,800}, Vector2{300,300}, Vector2{200,400}, YELLOW);
+}
+
 int main(int argc, char** argv) {
+    float content_scale = 10.0f;
     TraceLog(LOG_NONE, "The start of something great");
     const int width = 1280;
     const int height = 720;
@@ -11,7 +18,18 @@ int main(int argc, char** argv) {
     InitWindow(width, height, "term - gpu");
     SetTraceLogLevel(LOG_WARNING);
     bool running = true;
+    RenderTexture2D main_rt = LoadRenderTexture(GetRenderWidth(), GetRenderHeight());
+    Rectangle window_rect = {0,0,0,0};
+
+    // try shell here I guess
+
     while (running) {
+        // could move to if resized to save some performance
+        window_rect = Rectangle{0.0f, 0.0f, (float)GetRenderWidth(), (float)GetRenderHeight()};
+        if (IsWindowResized()) {
+            UnloadRenderTexture(main_rt);
+            main_rt = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
+        }
         if (WindowShouldClose()) {
             running = false;
         }
@@ -26,22 +44,17 @@ int main(int argc, char** argv) {
             SetWindowState(flags);
         }
 
-        Vector2 triangle_pos[] = {
-            Vector2{width / 2, height - (height / 3)*2},
-            Vector2{width / 3, height - (height / 3)},
-            Vector2{width - width / 3, height - (height / 3)},
-        };
-
         BeginDrawing();
         ClearBackground(BLACK);
 
-        // printf("0: (%.1f,%.1f), 1: (%.1f,%.1f) 2: (%.1f,%.1f)\n", triangle_pos[0].x, triangle_pos[0].y, triangle_pos[1].x, triangle_pos[1].y, triangle_pos[2].x, triangle_pos[2].y);
-        DrawTriangle(triangle_pos[0], triangle_pos[1], triangle_pos[2], BLUE);
-        DrawText(TextFormat("%d", GetFPS()), 4, 4, 16, WHITE);
+        BeginTextureMode(main_rt);
+        ClearBackground(BLACK);
+        draw_frame();
+        EndTextureMode();
+
+        DrawTexturePro(main_rt.texture, Rectangle{0,0,(float)main_rt.texture.width,(float)-main_rt.texture.height}, window_rect, Vector2{0.0f,0.0f}, 0.0f, WHITE);
+
         EndDrawing();
-
-
     }
     CloseWindow();
-
 }
